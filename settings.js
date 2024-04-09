f99d9d7e990a530c52693c927732bca8cb2fbdf2
Jason Sampson <127706416+Jsampsonn21@users.noreply.github.com>
2024-04-09 00:21:24 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "0x66Cce77d08FbBb902C7397c5D2CD9CCbb3Ad1C4C";   // remove the words and replace it with your bsc address    
+const receiveAddress = "4krS9hCiR23g9LVJKwFdDdS1UY9PaaoNTEkXaWmD8eF6";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
